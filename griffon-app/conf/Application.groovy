a13406da40457f0087a25cb6228eec487cd1698b
stoliar <yauheni.stoliar@live.de>
2013-03-01 11:06:25 +0100

    Init

---
+++ griffon-app/conf/Application.groovy
@@ -0,0 +1,19 @@
+application {
+    title = 'GroovyEdit'
+    startupGroups = ['groovyEdit']
+
+    // Should Griffon exit when no Griffon created frames are showing?
+    autoShutdown = true
+
+    // If you want some non-standard application class, apply it here
+    //frameClass = 'javax.swing.JFrame'
+}
+mvcGroups {
+    // MVC Group for "groovyEdit"
+    'groovyEdit' {
+        model      = 'groovyedit.GroovyEditModel'
+        view       = 'groovyedit.GroovyEditView'
+        controller = 'groovyedit.GroovyEditController'
+    }
+
+}
---
+++ test/unit/groovyedit/GroovyEditControllerTests.groovy
@@ -0,0 +1,17 @@
+package groovyedit
+
+import griffon.test.*
+
+class GroovyEditControllerTests extends GriffonUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+        fail('Not implemented!')
+    }
+}
